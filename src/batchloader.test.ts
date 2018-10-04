765840a7fdd32e7a227e0844b9318856c8f56c1b
Joon Ho Cho <joonho1101@gmail.com>
2018-10-03 22:31:55 -0700

    test coverage

--- src/batchloader.test.ts
+++ src/batchloader.test.ts
@@ -1,45 +1,98 @@
 import { BatchLoader } from './batchloader';
 
-test('BatchLoader', async () => {
-  const idss = [] as number[][];
-  const loader = new BatchLoader(
-    (ids: number[]): Promise<number[]> =>
-      new Promise(
-        (resolve): void => {
-          idss.push(ids);
-          setTimeout(() => resolve(ids.map((i) => i * 2)), 10);
-        }
-      ),
-    String
-  );
+describe('BatchLoader', () => {
+  test('with keyToUniqueId', async () => {
+    const idss = [] as number[][];
+    const loader = new BatchLoader(
+      (ids: number[]): Promise<number[]> =>
+        new Promise(
+          (resolve): void => {
+            idss.push(ids);
+            setTimeout(() => resolve(ids.map((i) => i * 2)), 10);
+          }
+        ),
+      String
+    );
 
-  expect(await loader.load(3)).toBe(6);
-  expect(await loader.load(4)).toBe(8);
-  expect(await loader.load(5)).toBe(10);
+    expect(await loader.load(3)).toBe(6);
+    expect(await loader.load(4)).toBe(8);
+    expect(await loader.load(5)).toBe(10);
 
-  expect(await loader.loadMany([])).toEqual([]);
-  expect(await loader.loadMany([1, 2, 3])).toEqual([2, 4, 6]);
-  expect(await loader.loadMany([1, 2, 3, 2, 3, 2, 1])).toEqual([
-    2,
-    4,
-    6,
-    4,
-    6,
-    4,
-    2,
-  ]);
+    expect(await loader.loadMany([])).toEqual([]);
+    expect(await loader.loadMany([1, 2, 3])).toEqual([2, 4, 6]);
+    expect(await loader.loadMany([1, 2, 3, 2, 3, 2, 1])).toEqual([
+      2,
+      4,
+      6,
+      4,
+      6,
+      4,
+      2,
+    ]);
 
-  expect(
-    await Promise.all([
-      loader.load(1),
-      loader.load(2),
-      loader.load(3),
-      loader.load(2),
-      loader.load(1),
-      loader.load(2),
-      loader.load(3),
-    ])
-  ).toEqual([2, 4, 6, 4, 2, 4, 6]);
+    expect(
+      await Promise.all([
+        loader.load(1),
+        loader.load(2),
+        loader.load(3),
+        loader.load(2),
+        loader.load(1),
+        loader.load(2),
+        loader.load(3),
+      ])
+    ).toEqual([2, 4, 6, 4, 2, 4, 6]);
 
-  expect(idss).toEqual([[3], [4], [5], [1, 2, 3], [1, 2, 3], [1, 2, 3]]);
+    expect(idss).toEqual([[3], [4], [5], [1, 2, 3], [1, 2, 3], [1, 2, 3]]);
+  });
+
+  test('without keyToUniqueId', async () => {
+    const idss = [] as number[][];
+    const loader = new BatchLoader(
+      (ids: number[]): Promise<number[]> =>
+        new Promise(
+          (resolve): void => {
+            idss.push(ids);
+            setTimeout(() => resolve(ids.map((i) => i * 2)), 10);
+          }
+        ),
+      null
+    );
+
+    expect(await loader.load(3)).toBe(6);
+    expect(await loader.load(4)).toBe(8);
+    expect(await loader.load(5)).toBe(10);
+
+    expect(await loader.loadMany([])).toEqual([]);
+    expect(await loader.loadMany([1, 2, 3])).toEqual([2, 4, 6]);
+    expect(await loader.loadMany([1, 2, 3, 2, 3, 2, 1])).toEqual([
+      2,
+      4,
+      6,
+      4,
+      6,
+      4,
+      2,
+    ]);
+
+    expect(
+      await Promise.all([
+        loader.load(1),
+        loader.load(2),
+        loader.load(3),
+        loader.load(2),
+        loader.load(1),
+        loader.load(2),
+        loader.load(3),
+      ])
+    ).toEqual([2, 4, 6, 4, 2, 4, 6]);
+
+    expect(idss).toEqual([
+      [3],
+      [4],
+      [5],
+      [1, 2, 3],
+      [1, 2, 3, 2, 3, 2, 1],
+      [1, 2, 3, 2, 1, 2, 3],
+    ]);
+  });
 });
--- src/mappedbatchloader.test.ts
+++ src/mappedbatchloader.test.ts
@@ -1,43 +1,86 @@
 import { BatchLoader } from './batchloader';
 import { MappedBatchLoader } from './mappedbatchloader';
 
-test('MappedBatchLoader', async () => {
-  const idss = [] as number[][];
-  const loader = new MappedBatchLoader(
-    new BatchLoader(
-      (ids: number[]): Promise<number[]> =>
-        new Promise(
-          (resolve): void => {
-            idss.push(ids);
-            setTimeout(() => resolve(ids.map((i) => i * 2)), 10);
-          }
-        ),
+describe('MappedBatchLoader', () => {
+  test('sync mapper', async () => {
+    const idss = [] as number[][];
+    const loader = new MappedBatchLoader(
+      new BatchLoader(
+        (ids: number[]): Promise<number[]> =>
+          new Promise(
+            (resolve): void => {
+              idss.push(ids);
+              setTimeout(() => resolve(ids.map((i) => i * 2)), 10);
+            }
+          ),
+        String
+      ),
       String
-    ),
-    String
-  );
+    );
 
-  expect(await loader.load(3)).toBe('6');
-  expect(await loader.load(4)).toBe('8');
-  expect(await loader.load(5)).toBe('10');
+    expect(await loader.load(3)).toBe('6');
+    expect(await loader.load(4)).toBe('8');
+    expect(await loader.load(5)).toBe('10');
 
-  expect(await loader.loadMany([])).toEqual([]);
-  expect(await loader.loadMany([1, 2, 3])).toEqual([2, 4, 6].map(String));
-  expect(await loader.loadMany([1, 2, 3, 2, 3, 2, 1])).toEqual(
-    [2, 4, 6, 4, 6, 4, 2].map(String)
-  );
+    expect(await loader.loadMany([])).toEqual([]);
+    expect(await loader.loadMany([1, 2, 3])).toEqual([2, 4, 6].map(String));
+    expect(await loader.loadMany([1, 2, 3, 2, 3, 2, 1])).toEqual(
+      [2, 4, 6, 4, 6, 4, 2].map(String)
+    );
 
-  expect(
-    await Promise.all([
-      loader.load(1),
-      loader.load(2),
-      loader.load(3),
-      loader.load(2),
-      loader.load(1),
-      loader.load(2),
-      loader.load(3),
-    ])
-  ).toEqual([2, 4, 6, 4, 2, 4, 6].map(String));
+    expect(
+      await Promise.all([
+        loader.load(1),
+        loader.load(2),
+        loader.load(3),
+        loader.load(2),
+        loader.load(1),
+        loader.load(2),
+        loader.load(3),
+      ])
+    ).toEqual([2, 4, 6, 4, 2, 4, 6].map(String));
 
-  expect(idss).toEqual([[3], [4], [5], [1, 2, 3], [1, 2, 3], [1, 2, 3]]);
+    expect(idss).toEqual([[3], [4], [5], [1, 2, 3], [1, 2, 3], [1, 2, 3]]);
+  });
+
+  test('async mapper', async () => {
+    const idss = [] as number[][];
+    const loader = new MappedBatchLoader(
+      new BatchLoader(
+        (ids: number[]): Promise<number[]> =>
+          new Promise(
+            (resolve): void => {
+              idss.push(ids);
+              setTimeout(() => resolve(ids.map((i) => i * 2)), 10);
+            }
+          ),
+        String
+      ),
+      (x): Promise<string> => Promise.resolve(String(x))
+    );
+
+    expect(await loader.load(3)).toBe('6');
+    expect(await loader.load(4)).toBe('8');
+    expect(await loader.load(5)).toBe('10');
+
+    expect(await loader.loadMany([])).toEqual([]);
+    expect(await loader.loadMany([1, 2, 3])).toEqual([2, 4, 6].map(String));
+    expect(await loader.loadMany([1, 2, 3, 2, 3, 2, 1])).toEqual(
+      [2, 4, 6, 4, 6, 4, 2].map(String)
+    );
+
+    expect(
+      await Promise.all([
+        loader.load(1),
+        loader.load(2),
+        loader.load(3),
+        loader.load(2),
+        loader.load(1),
+        loader.load(2),
+        loader.load(3),
+      ])
+    ).toEqual([2, 4, 6, 4, 2, 4, 6].map(String));
+
+    expect(idss).toEqual([[3], [4], [5], [1, 2, 3], [1, 2, 3], [1, 2, 3]]);
+  });
 });
