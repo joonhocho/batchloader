94f994c4fc03761f1454c67e470fc61dc003fe27
Joon Ho Cho <joonho1101@gmail.com>
2019-07-10 19:03:40 -0700

    index file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export * from './batchloader';
-export * from './mappedbatchloader';
-export * from './cacheproxyloader';
+export { BatchLoadFn, BatchLoader, KeyToUniqueId } from './batchloader';
+export { CacheLoader, ICache } from './cacheloader';
+export { IBatchCache, proxyLoaderWithCache } from './cacheproxyloader';
+export { MappedBatchLoader } from './mappedbatchloader';
+export { IBatchLoader, MaybePromise } from './types';
